40cb90929b49eb4b5a964848c6c8215cd535f5e1
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-13 18:07:46 +0200

    Production Version #1

---
+++ src/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function getTheNumber (theNumber, decision = '', startNumber = 1) {
+    
+    if (theNumber <= 0 || typeof theNumber !== 'number') {
+        return `Invalid input information.`;
+    }; 
+
+    if (startNumber === theNumber) {
+        return `${theNumber} = ${decision}`;
+    };
+    
+    if (startNumber < theNumber) {
+        const variant1 = getTheNumber(theNumber, `(${decision}${startNumber} + 5)`, startNumber + 5);
+        const variant2 = getTheNumber(theNumber, `(${decision}${startNumber} * 3)`, startNumber * 3);    
+        return variant1 || variant2;
+    };
+
+    return null;
+};
+
+console.log(getTheNumber(13));
